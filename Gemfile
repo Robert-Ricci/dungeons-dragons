0d63035d7aab3a6ec15d81a9e07232847014bc87
Robert Ricci <rmricci121@gmail.com>
2021-03-01 15:49:51 -0500

    This commit generatered a character model and controller

--- Gemfile
+++ Gemfile
@@ -33,6 +33,14 @@ gem 'jquery-rails'
 gem 'devise'
 gem 'bootstrap_form'
 
+# gem 'rspec-rails', '~> 3.6'
+# gem 'factory_girl_rails'
+# gem 'rails-controller-testing'
+# gem 'headless'
+# gem 'capybara'
+# gem 'poltergeist'
+# gem 'database_cleaner'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_194655) do
+ActiveRecord::Schema.define(version: 2021_03_01_204312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.string "race"
+    t.string "klass"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
