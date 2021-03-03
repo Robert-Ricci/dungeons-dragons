9d1de3941777b0fa40bde4dc755068f066de7f08
Robert Ricci <rmricci121@gmail.com>
2021-03-03 13:38:04 -0500

    This commit started seting up characters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,13 +1,41 @@
 class CharactersController < ApplicationController
-  def index
-  end
-
   def new
+    @character = Character.new
   end
 
   def create
+    if current_user
+    @character = Character.create(character_params)
+    # binding.pry
+    @character.user_id = current_user.id
+
+   
+
+    if @character.save
+      redirect_to user_characters_path(@character)
+    else 
+      render 'new'
+    end
+  end
+  end
+
+  def index
+    @characters = Character.all
   end
 
   def show
+    @character = Character.find_by(id: params[:id])
   end
+
+  def destroy
+    Character.find(params[:id]).destroy
+    redirect_to characters_url
+  end
+
+  private
+
+  def character_params
+    params.require(:character).permit(:name, :klass, :race_name )
+  end
+
 end
