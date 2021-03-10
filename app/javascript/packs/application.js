df7a3a3040d2000838fd3cb6743d7d1517c64ca0
Robert Ricci <rmricci121@gmail.com>
2021-03-10 10:28:07 -0500

    This commit seems to have finally fixed my issue with my nav-bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-// require("bootstrap/dropdown")
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
 
-require("jquery")
-require("bootstrap-sprockets")
+//= require jquery
+//= require jquery-ui
+//= require popper
+//= require bootstrap-sprockets
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
 
 //= require jquery
 //= require jquery_ujs
@@ -18,6 +19,7 @@ require("bootstrap-sprockets")
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
